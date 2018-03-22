5d0898d9d28014a2c23508fbe5b870380932c368
jxmot <webdev@stupiddog.net>
2018-03-21 23:28:20 -0500

    initial commit, get repo info

---
+++ www/php/findrepo.php
@@ -0,0 +1,39 @@
+<?php
+/*
+    getrepoinfo.php?r=repo_name&f=../data/_owner-repos.json
+
+    for command line testing :
+
+        php-cgi findrepo.php r=repo_name f=../data/_owner-repos.json
+*/
+$reporeq  = $_REQUEST["r"];
+$repofile = $_REQUEST["f"];
+$allrepos = null;
+$resp = null;
+$repojson = null;
+
+if(isset($reporeq) && isset($repofile)) {
+    if(file_exists($repofile)) {
+        $allrepos = json_decode(file_get_contents($repofile));
+        $repolen = count($allrepos);
+        for($ix = 0; $ix < $repolen; $ix += 1) {
+            if(strtolower($allrepos[$ix]->name) === strtolower($reporeq)) {
+                //$repojson = stripslashes(json_encode($allrepos[$ix]));
+                $repojson = json_encode($allrepos[$ix]);
+                $resp = "{\"error\":false, \"ret\":0, \"msg\":$repojson}";
+//error_log("resp = $resp\n", 3, "./error_log");
+                break;
+            }
+        }
+    } else {
+        $resp = "{\"error\":true, \"ret\":-1, \"msg\":\"$repofile does not exist\"}";
+    }
+} else {
+    $resp = "{\"error\":true, \"ret\":-3, \"msg\":\"missing one or more argument(s)\"}";
+}
+header("HTTP/1.0 200 OK");
+header("Content-Type: application/json; charset=utf-8");
+header("Content-Encoding: text");
+echo $resp;
+exit;
+?>
---
+++ www/php/getrepoinfo.php
@@ -0,0 +1,64 @@
+<?php
+require_once "parseHeaders.php";
+$cfgfile = "../data/_gitlabels.json";
+$cfg = null;
+/*
+    getrepoinfo.php?r=repo_name
+
+    for command line testing :
+
+        php-cgi getrepoinfo.php r=repo_name
+*/
+$reporeq = $_REQUEST["r"];
+$repofile = null;
+$resp = null;
+$repojson = null;
+$foundrepo = null;
+
+if(isset($reporeq)) {
+    if(file_exists($cfgfile)) {
+        $cfg = json_decode(file_get_contents($cfgfile));
+        $repofile = "../data/_$cfg->owner-repos.json";
+
+        if(file_exists($repofile)) {
+            $repojson = file_get_contents($repofile);
+            if(isset($repojson) && (gettype($repojson) === "string")) {
+                if(strpos(strtolower($repojson), strtolower($reporeq))) {
+                    // found the repo name in the JSON string, now find
+                    // the repo object by converting the string to an array
+                    // and then walk through the array looking for a match.
+                    $foundrepo = shell_exec("php-cgi ./findrepo.php r=".$reporeq." f=".$repofile);
+                    $realrepo = substr($foundrepo, strpos($foundrepo, "{"));
+                    $thisrepo = json_decode($realrepo);
+
+                    if(($thisrepo->error === false) && ($thisrepo->ret >= 0)) {
+                        $justrepo = $thisrepo->msg;
+                        $fork = ($justrepo->fork ? 'true' : 'false');
+                        $private = ($justrepo->private ? 'true' : 'false');
+                        $tmp = "{\"name\":\"$justrepo->name\",\"fullname\":\"$justrepo->full_name\",\"fork\":$fork,\"forks\":$justrepo->forks,\"private\":$private,\"open_issues\":$justrepo->open_issues}";
+                        $resp = "{\"error\":false, \"ret\":0, \"msg\":$tmp}";
+                    } else {
+                        $resp = "{\"error\":true, \"ret\":$thisrepo->ret, \"msg\":\"$thisrepo->msg\"}";
+                    }
+                } else {
+                    $resp = "{\"error\":true, \"ret\":-5, \"msg\":\"$reporeq not found in $repofile\"}";
+                }
+            } else {
+                $resp = "{\"error\":true, \"ret\":-4, \"msg\":\"missing ../data/_$cfg->owner-repos.json\"}";
+            }
+        } else {
+            $resp = "{\"error\":true, \"ret\":-4, \"msg\":\"missing ../data/_$cfg->owner-repos.json\"}";
+        }
+    } else {
+        $resp = "{\"error\":true, \"ret\":-1, \"msg\":\"$cfgfile does not exist\"}";
+    }
+} else {
+    $resp = "{\"error\":true, \"ret\":-3, \"msg\":\"argument missing, expecting - '?r=repo_name'\"}";
+}
+header("HTTP/1.0 200 OK");
+header("Content-Type: application/json; charset=utf-8");
+header("Content-Encoding: text");
+echo stripslashes($resp);;
+exit;
+
+?>
