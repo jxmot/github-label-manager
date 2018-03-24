0aa6f13516c2a1c38b5778818d731d3e7cbbc5f6
jxmot <webdev@stupiddog.net>
2018-03-24 01:38:39 -0500

    initial commit, common location for config file path+name & accept heads

---
+++ www/php/ghlabelmgr.php
@@ -0,0 +1,7 @@
+<?php
+$cfgfile = "../data/_gitlabels.json";
+$accept = array(
+    'symmetra' => 'application/vnd.github.symmetra-preview+json',
+    'mercy' => 'application/vnd.github.mercy-preview+json'
+);
+?>
