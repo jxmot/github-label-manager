36b9f82a79bcf806fce6f1e05737f9a7c417acef
jxmot <webdev@stupiddog.net>
2018-03-24 15:47:34 -0500

    refactored HTTP GET code

--- www/php/getrepos.php
+++ www/php/getrepos.php
@@ -1,7 +1,7 @@
 <?php
 // contained in "ghlabelmgr.php" - $cfgfile, $accept
 require_once "ghlabelmgr.php";
-require_once "parseHeaders.php";
+require_once "delete-get.php";
 
 /*
     GET getrepos.php
@@ -13,37 +13,21 @@ require_once "parseHeaders.php";
 $resp = null;
 if(file_exists($cfgfile)) {
     $cfg = json_decode(file_get_contents($cfgfile));
-
     // https://developer.github.com/v3/guides/traversing-with-pagination/
     $url = "https://api.github.com/users/$cfg->owner/repos?per_page=100";
-    
-    $opts = array(
-        'http' => array(
-            'method' => 'GET',
-            'header' => "Accept: ".$accept['mercy']."\r\n" .
-            "user-agent: custom\r\n" .
-            "Content-Type: application/json; charset=utf-8\r\n" .
-            "Content-Encoding: text\r\n"
-        )
-    );
-    
-    $context = stream_context_create($opts);
-    $resp = file_get_contents($url, true, $context);
-    $pheader = parseHeaders($http_response_header);
-
-    if(strpos(strtolower($pheader[0]), "200 ok")) {
+    $acc = $accept['mercy'];
+    $resp = get($url, $acc, $cfg);
+    $tmp = json_decode($resp);
+    if($tmp->error === false) {
         $fname = "../data/_$cfg->owner-repos.json";
         file_put_contents($fname, $resp);
-
         $repodisp = array();
-        $allrepos = json_decode($resp);
+        $allrepos = $tmp->msg;
         for($ix = 0;$ix < count($allrepos);$ix += 1) {
             $tmp = "{\"name\":\"".$allrepos[$ix]->name."\",\"full_name\":\"".$allrepos[$ix]->full_name."\"}";
             $repodisp[$ix] = json_decode($tmp);
         }
-        $resp = "{\"error\":false, \"ret\":0, \"msg\":". stripslashes(json_encode($repodisp)) ."}";
-    } else {
-        $resp = "{\"error\":true, \"ret\":-1, \"msg\":\"response = $pheader[0]\"}";
+        $resp = "{\"error\":false, \"ret\":0, \"msg\":".json_encode($repodisp)."}";
     }
 } else {
     $resp = "{\"error\":true, \"ret\":-2, \"msg\":\"$cfgfile does not exist\"}";
