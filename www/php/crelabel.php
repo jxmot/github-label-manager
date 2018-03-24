901264b54b09ff234b90d8c197ac70c6da69bd79
jxmot <webdev@stupiddog.net>
2018-03-24 15:32:07 -0500

    refactored HTTP POST code

--- www/php/crelabel.php
+++ www/php/crelabel.php
@@ -1,13 +1,14 @@
 <?php
 // contained in "ghlabelmgr.php" - $cfgfile, $accept
 require_once "ghlabelmgr.php";
-require_once "parseHeaders.php";
+require_once "patch-post.php";
 
 /*
     POST crelabel.php
     body: {"repo":"repo name", "label":{"name":"labelName", "color":"7f7fff", "description":"test label, CREATED"}}
 */
 $resp = null;
+
 // get the BODY from the POST
 $postbody = file_get_contents('php://input');
 // JSON -> associative array
@@ -21,25 +22,8 @@ if((isset($labelrepo)) && (isset($label))) {
     if(file_exists($cfgfile)) {
         $cfg = json_decode(file_get_contents($cfgfile));
         $url = "https://api.github.com/repos/$cfg->owner/$labelrepo/labels";
-        $opts = array(
-            'http' => array(
-                'method' => 'POST',
-                'header' => "Accept: ".$accept['symmetra']."\r\n" .
-                "Authorization: $cfg->token\r\n" .
-                "user-agent: custom\r\n" .
-                "Content-Type: application/json; charset=utf-8\r\n" .
-                "Content-Encoding: text\r\n",
-                'content' => $label
-            )
-        );
-        $context = stream_context_create($opts);
-        $newlabel = file_get_contents($url, true, $context);
-        $pheader = parseHeaders($http_response_header);
-        if(strpos(strtolower($pheader[0]), "201 created")) {
-            $resp = "{\"error\":false, \"ret\":0, \"msg\":$newlabel}";
-        } else {
-            $resp = "{\"error\":true, \"ret\":-1, \"msg\":\"response = $pheader[0]\"}";
-        }
+        $acc = $accept['symmetra'];
+        $resp = post($url, $acc, $cfg, $label);
     } else {
         $resp = "{\"error\":true, \"ret\":-2, \"msg\":\"$cfgfile does not exist\"}";
     }
