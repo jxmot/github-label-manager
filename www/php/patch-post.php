2d0665358c35acb2c88aa85f7ccaba420a2a5ca3
jxmot <webdev@stupiddog.net>
2018-03-24 15:29:54 -0500

    refactored HTTP PATCH/POSTcode into a single primary function

---
+++ www/php/patch-post.php
@@ -0,0 +1,40 @@
+<?php
+require_once "parseHeaders.php";
+
+function patch($url, $acc, $cfg, $payload)
+{
+    $resp = request_pp($url, 'PATCH', $acc, $cfg, $payload);
+    return $resp;
+}
+
+function post($url, $acc, $cfg, $payload)
+{
+    $resp = request_pp($url, 'POST', $acc, $cfg, $payload);
+    return $resp;
+}
+
+function request_pp($url, $method, $acc, $cfg, $payload)
+{
+    $opts = array(
+        'http' => array(
+            'method' => $method,
+            'header' => "Accept: $acc\r\n" .
+            "Authorization: $cfg->token\r\n" .
+            "user-agent: custom\r\n" .
+            "Content-Type: application/json; charset=utf-8\r\n" .
+            "Content-Encoding: text\r\n",
+            'content' => $payload
+        )
+    );
+    $context = stream_context_create($opts);
+    $srvresp = file_get_contents($url, false, $context);
+    $pheader = parseHeaders($http_response_header);
+    $status  = ($method === 'PATCH' ? "200 ok" : ($method === 'POST' ? "201 created" : "UNKNOWN"));
+    if(strpos(strtolower($pheader[0]), $status)) {
+        $resp = "{\"error\":false, \"ret\":0, \"msg\":$srvresp}";
+    } else {
+        $resp = "{\"error\":true, \"ret\":-1, \"msg\":\"response = $pheader[0]\"}";
+    }
+    return $resp;
+}
+?>
