ad390b0d6ac96626dfaa36068c75215d83cebb97
jxmot <webdev@stupiddog.net>
2018-04-09 17:39:45 -0500

    added import label preview

--- www/assets/js/modal-import.js
+++ www/assets/js/modal-import.js
@@ -17,6 +17,13 @@ var     importCancel = false;
 $('#import-labels-btn').on('click', confirmImport);
 $('#import-cancel-btn').on('click', cancelImport);
 
+// file list events
+function fileListClick(fileid) {
+    var file = document.getElementById(fileid).dataset.file;
+    $('#import-file-name').val(file);
+    getimportlabels(file, showImportLabels);
+};
+
 /*
     The button assigned to the "confirm" functionality is handled here.
 */
@@ -49,6 +56,12 @@ $('#labelImportModal').on('hide.bs.modal', function (event) {
         // by the user.
         startShake('#labelImportModal');
     } else {
+        if((importLabels === true) && (importCancel === false)) {
+        } else {
+            if((importLabels === false) && (importCancel === true)) {
+                consolelog('import cancelled');
+            }
+        }
     }
 });
 
@@ -64,7 +77,50 @@ $('#labelImportModal').on('show.bs.modal', function (event) {
 /*
     Pre-fill the label-file list before showing the modal.
 */
-function fillImport(resp) {
+function fillImport(files) {
+    $('#filelist').empty();
+    $('#import-labels-list-body').empty();
+    $('#import-file-name').val('None Selected');
 
+    if((files.error === false) && (files.ret > 0)) {
+        for(var ix = 0;ix < files.msg.length;ix += 1) {
+            var file = $('<li>');
+            $(file).attr('id', 'file-'+ix);
+            $(file).addClass('import-file-item');
+            $(file).attr('onclick','fileListClick(this.id)'); 
+            $(file).attr('data-file', files.msg[ix]);
+
+            var text = $('<strong>').text(files.msg[ix]);
+            $(file).append(text);
+
+            $('#filelist').append(file);
+        }
+    }
 };
+
+function showImportLabels(labels) {
+    $('#import-labels-list-body').empty();
+
+    if((labels.error === false) && (labels.ret >= 0)) {
+        if(labels.msg.length > 1) $('#import-label-col').text(labels.msg.length + ' Labels');
+        else if(labels.msg.length > 0) $('#import-label-col').text(labels.msg.length + ' Label');
+        else $('#import-label-col').text('Label');
+
+        for(var ix = 0;ix < labels.msg.length;ix += 1) {
+            var row = $('<tr>');
+            const lbldata = JSON.parse(JSON.stringify(labels.msg[ix]));
+            var cell = $('<td>').addClass('table-cell-center');
+            var label = $('<span>').addClass('label label-default');
+            $(label).text(lbldata.name);
+            $(label).attr('style', 'background-color:#'+lbldata.color+';color:#'+adaptColor(lbldata.color)+';');
+            $(cell).append($('<h3>').addClass('label-header').append(label));
+            $(row).append(cell);
+            cell = $('<td>').text((lbldata.description === null ? '' : lbldata.description));
+            $(row).append(cell);
+            $('#import-labels-list-body').append(row);
+        }
+    }
+};
+
+
 
