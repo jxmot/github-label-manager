d35fc5cf256ba6cf7256a0b8cc4af436aa9af51c
jxmot <webdev@stupiddog.net>
2018-04-09 23:43:39 -0500

    refactored listLabels() into 2 funcs , listLabels() and _listLabels()

--- www/assets/js/labels.js
+++ www/assets/js/labels.js
@@ -103,84 +103,95 @@ function filesdone(resp) {
 
 function listLabels(labels) {
     if(labels.error === false) {
-        
-        if(labels.msg.length > 1) $('#table-label-col').text(labels.msg.length + ' Labels');
-        else if(labels.msg.length > 0) $('#table-label-col').text(labels.msg.length + ' Label');
-        else $('#table-label-col').text('Label');
+        _listLabels(labels.msg);
+    }
+};
+
+
+function _listLabels(labels, labelimport = false) {
+    if(labels.length > 1) $('#table-label-col').text(labels.length + ' Labels');
+    else if(labels.length > 0) $('#table-label-col').text(labels.length + ' Label');
+    else $('#table-label-col').text('Label');
 
 // TODO: save a separate ix for only element IDs across reads, imports, 
 // etc and manage largest allowed value
-        for(var ix = 0;ix < labels.msg.length;ix += 1) {
-            var name = labels.msg[ix].name.replace(/ /g, '_');
 
-            var nameix = name+'-'+ix;
-            const lbldata = JSON.parse(JSON.stringify(labels.msg[ix]));
+    for(var ix = 0;ix < labels.length;ix += 1) {
+        var name = labels[ix].name.replace(/ /g, '_');
 
-            var row = $('<tr>');
-            $(row).attr('id', nameix);
-            var label_now = {
-                label:lbldata,
-                chksum:checksum(JSON.stringify(lbldata))
-            };
-            $(row).attr('data-label_ro', JSON.stringify(label_now));
-            $(row).attr('data-label_rw', JSON.stringify(label_now));
-            $(row).attr('data-enact', '{"edit":true,"del":true,"undo":false}');
-            // states: 1 = unchgd  2 = chgd  4 = del
-            // multi states: 
-            //      1 + 4 = unchgd and del
-            //      2 + 4 = chgd and del
-            // unmarking del subs 4
-            $(row).attr('data-state', '{"state":1}');
+        var nameix = name+'-'+ix;
+        const lbldata = JSON.parse(JSON.stringify(labels[ix]));
 
-            var cell = $('<td>').addClass('table-cell-center');
-            var label = $('<span>').attr('id', nameix+'-color').addClass('label label-default');
-            $(label).text(lbldata.name);
-            $(label).attr('style', 'background-color:#'+lbldata.color+';color:#'+adaptColor(lbldata.color)+';');
-            $(cell).append($('<h3>').addClass('label-header').append(label));
-            $(row).append(cell);
+        var row = $('<tr>');
+        $(row).attr('id', nameix);
+        var label_now = {
+            label:lbldata,
+            chksum:checksum(JSON.stringify(lbldata))
+        };
+        $(row).attr('data-label_ro', JSON.stringify(label_now));
+        $(row).attr('data-label_rw', JSON.stringify(label_now));
+        $(row).attr('data-enact', '{"edit":true,"del":true,"undo":false}');
+        // states: 1 = unchgd  2 = chgd  4 = del
+        // multi states: 
+        //      1 + 4 = unchgd and del
+        //      2 + 4 = chgd and del
+        // unmarking del subs 4
+        $(row).attr('data-state', '{"state":1}');
 
-            cell = $('<td>').attr('id', nameix+'-desc').text((lbldata.description === null ? '' : lbldata.description));
-            $(row).append(cell);
+        var cell = $('<td>').addClass('table-cell-center');
+        var label = $('<span>').attr('id', nameix+'-color').addClass('label label-default');
+        $(label).text(lbldata.name);
+        $(label).attr('style', 'background-color:#'+lbldata.color+';color:#'+adaptColor(lbldata.color)+';');
+        $(cell).append($('<h3>').addClass('label-header').append(label));
+        $(row).append(cell);
 
-            cell = $('<td>').addClass('table-cell-center');
-            var actions = $('<div>');
-            var edit = $('<span>').addClass('fas fa-edit fa-lg label-edit-icon');
-            var del = $('<span>').addClass('fas fa-trash fa-lg label-delete-icon');
-            var undo = $('<span>').addClass('fas fa-undo fa-lg label-undo-icon icon-disabled');
-            $(edit).attr('data-action','edit'); 
-            $(del). attr('data-action','delete');
-            $(undo).attr('data-action','undo');
-            $(edit).attr('id', nameix+'-'+'edit');
-            $(del).attr('id', nameix+'-'+'del');
-            $(undo).attr('id', nameix+'-'+'undo');
-            $(edit).attr('onclick','labelAction(this.id)'); 
-            $(del). attr('onclick','labelAction(this.id)');
-            $(undo).attr('onclick','labelAction(this.id)');
-            $(actions).append(edit);
-            $(actions).append(del);
-            $(actions).append(undo);
-            $(cell).append(actions);
-            $(row).append(cell);
+        cell = $('<td>').attr('id', nameix+'-desc').text((lbldata.description === null ? '' : lbldata.description));
+        $(row).append(cell);
 
-            cell = $('<td>').addClass('table-cell-center');
-            var states = $('<div>');
-            var notmod = $('<span>').addClass('fas fa-check-circle fa-lg label-notmod-icon');
-            var tomod  = $('<span>').addClass('fas fa-exclamation-triangle fa-lg label-ismod-icon hidden');
-            var todel  = $('<span>').addClass('fas fa-trash-alt fa-lg label-to-delete-icon hidden');
-            $(notmod).attr('data-state','notmod'); 
-            $(tomod).attr('data-state','tomod');
-            $(todel).attr('data-state','todel');
-            $(notmod).attr('id', nameix+'-'+'notmod');
-            $(tomod).attr('id', nameix+'-'+'tomod');
-            $(todel).attr('id', nameix+'-'+'todel');
-            $(states).append(notmod);
-            $(states).append(tomod);
-            $(states).append(todel);
-            $(cell).append(states);
-            $(row).append(cell);
+        cell = $('<td>').addClass('table-cell-center');
+        var actions = $('<div>');
+        var edit = $('<span>').addClass('fas fa-edit fa-lg label-edit-icon');
+        var del = $('<span>').addClass('fas fa-trash fa-lg label-delete-icon');
+        var undo = $('<span>').addClass('fas fa-undo fa-lg label-undo-icon icon-disabled');
+        $(edit).attr('data-action','edit'); 
+        $(del). attr('data-action','delete');
+        $(undo).attr('data-action','undo');
+        $(edit).attr('id', nameix+'-'+'edit');
+        $(del).attr('id', nameix+'-'+'del');
+        $(undo).attr('id', nameix+'-'+'undo');
+        $(edit).attr('onclick','labelAction(this.id)'); 
+        $(del). attr('onclick','labelAction(this.id)');
+        $(undo).attr('onclick','labelAction(this.id)');
+        $(actions).append(edit);
+        $(actions).append(del);
+        $(actions).append(undo);
+        $(cell).append(actions);
+        $(row).append(cell);
 
-            $('#repo-labels-list-body').append(row);
+        cell = $('<td>').addClass('table-cell-center');
+        var states = $('<div>');
+        var notmod = $('<span>');
+        if(labelimport === true) {
+            $(notmod).addClass('fas fa-info-circle fa-lg label-notmod-icon');
+        } else {
+            $(notmod).addClass('fas fa-check-circle fa-lg label-notmod-icon');
         }
+
+        var tomod  = $('<span>').addClass('fas fa-exclamation-triangle fa-lg label-ismod-icon hidden');
+        var todel  = $('<span>').addClass('fas fa-trash-alt fa-lg label-to-delete-icon hidden');
+        $(notmod).attr('data-state','notmod'); 
+        $(tomod).attr('data-state','tomod');
+        $(todel).attr('data-state','todel');
+        $(notmod).attr('id', nameix+'-'+'notmod');
+        $(tomod).attr('id', nameix+'-'+'tomod');
+        $(todel).attr('id', nameix+'-'+'todel');
+        $(states).append(notmod);
+        $(states).append(tomod);
+        $(states).append(todel);
+        $(cell).append(states);
+        $(row).append(cell);
+
+        $('#repo-labels-list-body').append(row);
     }
 };
 
