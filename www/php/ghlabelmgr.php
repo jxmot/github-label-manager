7b6265cf2a99a8c949fb4c4ceb3c29c1bb18665d
jxmot <webdev@stupiddog.net>
2020-08-28 09:47:58 -0500

    added "v3" header accept option

--- www/php/ghlabelmgr.php
+++ www/php/ghlabelmgr.php
@@ -2,7 +2,8 @@
 $cfgfile = "../data/config/_gitlabels.json";
 $accept = array(
     'symmetra' => 'application/vnd.github.symmetra-preview+json',
-    'mercy' => 'application/vnd.github.mercy-preview+json'
+    'mercy' => 'application/vnd.github.mercy-preview+json',
+    'v3' => 'application/vnd.github.v3+json'
 );
 $maxoutsize = 20480;
 $imexportpath = "../data/labels";
